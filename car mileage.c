f18f1adf128b8f28be4fb15ca15dc8d67a1ba9d0
J3ffMwangi <mwasjeffrey3@gmail.com>
2025-02-02 11:09:13 +0300

    first commit

---
+++ car mileage.c
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+class Car {
+public:
+    string brand;
+    string model;
+    float price;
+    int mileage;
+
+
+    Car(string b, string m, float p, int mil) {
+        brand = b;
+        model = m;
+        price = p;
+        mileage = mil;
+    }
+
+
+    void display() {
+        cout << "Brand: " << brand << endl;
+        cout << "Model: " << model << endl;
+        cout << "Price: $" << price << endl;
+        cout << "Mileage: " << mileage << " miles" << endl;
+    }
+
+
+    void drive(int distance) {
+        mileage += distance;
+        cout << "Car driven for " << distance << " miles. Updated mileage: " << mileage << " miles" << endl;
+    }
+}
+
+int main() {
+ Car myCar("Toyota", 20000);
+
+    cout << "\nDriving the car...\n";
+
+   myCar.drive(150);
+
+
+    return 0;
+}
